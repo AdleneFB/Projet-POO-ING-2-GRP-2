20c5aced45050a99a015a83e8096052bc5b56942
adlene <farsadlene@gmail.com>
2025-12-19 17:55:28 +0100

    Add ControlCenter with drone assignment and cost calculation logic

---
+++ projetpoo/src/control/controlcenter.java
@@ -0,0 +1,13 @@
+package control;
+import java.util.ArrayList;
+import java.util.List;
+
+import Drone_Hierarchy.Drone;
+import geographical_method.Mapc;
+import geographical_method.position;
+import sys_Order.Order;
+
+public class controlcenter {
+	
+
+}
