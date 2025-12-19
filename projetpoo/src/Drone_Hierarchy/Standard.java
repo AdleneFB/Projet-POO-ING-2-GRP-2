86ddf3a5965d7e00f950a4da5e6e50d73c4ca521
dell <dell@lat35>
2025-12-19 17:42:38 +0100

    add standerd class for drones

--- projetpoo/src/Drone_Hierarchy/Standard.java
+++ projetpoo/src/Drone_Hierarchy/Standard.java
@@ -1,5 +1,14 @@
 package Drone_Hierarchy;
+import geographical_method.position;
 
-public class Standard {
+public class Standard extends Drone{
+	 private static final double CONSUMPTION_RATE = 3.0;
+	 public Standard(position position) {
+	        super(position, "StandardDrone", 30.0, 1.0);
+	    }
+	 @Override
+	    public double calculateConsumption(double distance) {
+	        return distance * CONSUMPTION_RATE;
+	    }
 
 }
