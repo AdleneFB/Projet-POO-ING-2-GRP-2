b9cb15a4b30234c0e2f3b96e587e081ed0e71de9
adlene <farsadlene@gmail.com>
2025-12-19 18:32:34 +0100

    Add Main class to run simulation

---
+++ projetpoo/src/main/Main.java
@@ -0,0 +1,32 @@
+package main;
+import control.controlcenter;
+import Drone_Hierarchy.Drone;
+import geographical_method.Mapc;
+import geographical_method.position;
+import sys_Order.Order;
+import sys_Order.StandardPackage;
+import simulation.simulator;
+
+public class main {
+	public static void main(String[] args) {
+
+        position base = new position(0, 0);
+        Mapc map = new Mapc();
+
+        controlcenter controlcenter = new controlcenter(base, map);
+
+        controlcenter.addDrone(new Standard(base));
+
+        Order order = new Order(
+                "Client A",
+                new StandardPackage(0.5, new position(10, 5)),
+                "NORMAL",
+                100
+        );
+
+        controlcenter.assignOrder(order);
+
+        simulator simulator = new simulator(controlcenter);
+        simulator.run();
+    }
+}
