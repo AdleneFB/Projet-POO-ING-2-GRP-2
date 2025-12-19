262b8da78e965c92b7ffce0b17fb481047a62db4
adlene <farsadlene@gmail.com>
2025-12-19 17:39:10 +0100

    Add Order class with status and urgency management

---
+++ projetpoo/src/sys_Order/Order.java
@@ -0,0 +1,15 @@
+package sys_Order;
+import geographical_method.position;
+
+public class Order {
+	 private double weight;
+	 private position destination;
+	 
+	 public StandardPackage(double weight, Position destination) {
+	        this.weight = weight;
+	        this.destination = destination;
+	    }
+	 
+	
+
+}
